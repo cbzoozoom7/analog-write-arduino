500fab6af3768f8bbde24773b6093ad65b002537
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2024-02-20 09:51:42 -0600

    Add files via upload

---
+++ AnalogWrite_assignment.ino
@@ -0,0 +1,17 @@
+int ledPin = 3;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledPin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int ledBrightness = 0;ledBrightness < 255; ledBrightness++) {
+analogWrite(ledPin,ledBrightness);
+delay(10);
+  }
+  for(int ledBrightness = 255;ledBrightness > 0;ledBrightness--) {
+  analogWrite(ledPin,ledBrightness);
+delay(10);
+  }
+}
